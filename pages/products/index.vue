b901d687bf3edd85b244af5a70cdda40aea490f6
Ralph Hernandez <ralph@Ralphs-MacBook-Air.local>
2023-05-19 16:47:48 +0800

    first commit

---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+            <ProductCard :product="p" />
+            </div>
+        </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'products'
+    })
+
+    //fetch products
+    const {data: products } = await useFetch('https://fakestoreapi.com/products/')
+
+</script>
+
+<style scoped>
+
+</style>
